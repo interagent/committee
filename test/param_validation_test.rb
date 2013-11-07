024248e2666b2f9c71a15ab0b426d17ea080d63f
Brandur <brandur@mutelight.org>
2013-11-07 14:32:43 -0800

    Add real tests for the parameter validation middleware

--- test/param_validation_test.rb
+++ test/param_validation_test.rb
@@ -14,7 +14,35 @@ describe Rack::Committee::ParamValidation do
     App
   end
 
-  it "x" do
-    post "/apps"
+  before do
+    header "Content-Type", "application/json"
+  end
+
+  it "detects an invalid Content-Type" do
+    header "Content-Type", "application/whats-this"
+    post "/account/app-transfers", "{}"
+    assert_equal 400, last_response.status
+  end
+
+  it "passes through a valid request" do
+    params = {
+      "app" => "www",
+      "recipient" => "owner@heroku.com",
+    }
+    post "/account/app-transfers", MultiJson.encode(params)
+    assert_equal 200, last_response.status
+  end
+
+  it "detects a missing parameter" do
+    post "/account/app-transfers", "{}"
+    assert_equal 422, last_response.status
+  end
+
+  it "detects an extra parameter" do
+    params = {
+      "cloud" => "production"
+    }
+    post "/account/app-transfers", MultiJson.encode(params)
+    assert_equal 422, last_response.status
   end
 end
