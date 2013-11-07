9035039c06129c1d40ba81f4d83aa63cf076db7b
Brandur <brandur@mutelight.org>
2013-11-07 14:26:42 -0800

    Use app-transfer JSON from schema.json

--- test/param_validator_test.rb
+++ test/param_validator_test.rb
@@ -1,18 +1,23 @@
 require_relative "test_helper"
 
 describe Rack::Committee::ParamValidator do
+  before do
+    data = MultiJson.decode(File.read("./test/schema.json"))
+    @link_schema = data["definitions"]["app-transfer"]["links"][0]
+  end
+
   it "passes through a valid request" do
     params = {
       "app" => "www",
       "recipient" => "owner@heroku.com",
     }
-    Rack::Committee::ParamValidator.new(params, link_schema).call
+    Rack::Committee::ParamValidator.new(params, @link_schema).call
   end
 
   it "detects a missing parameter" do
     message = "Missing params: app, recipient."
     assert_raises(Rack::Committee::InvalidParams) do
-      Rack::Committee::ParamValidator.new({}, link_schema).call
+      Rack::Committee::ParamValidator.new({}, @link_schema).call
     end
   end
 
@@ -22,24 +27,7 @@ describe Rack::Committee::ParamValidator do
     }
     message = "Unknown params: cloud."
     assert_raises(Rack::Committee::InvalidParams, message) do
-      Rack::Committee::ParamValidator.new(params, link_schema).call
+      Rack::Committee::ParamValidator.new(params, @link_schema).call
     end
-  end
-
-  def link_schema
-    {
-      "description"  => "Create a new app transfer.",
-      "href"         => "/account/app-transfers",
-      "method"       => "POST",
-      "rel"          => "create",
-      "schema"       => {
-        "properties" => {
-          "app"       => { "$ref" => "/schema/app#/definitions/identity" },
-          "recipient" => { "$ref" => "/schema/account#/definitions/identity" }
-        },
-        "required" => ["app","recipient"]
-      },
-      "title"        => "Create"
-    }
   end
 end
