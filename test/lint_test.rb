61bf9b660c8d22fe327ff8965fe23eed9a41bfcc
Brandur <brandur@mutelight.org>
2013-11-07 16:11:09 -0800

    Get rid of global vars

--- test/lint_test.rb
+++ test/lint_test.rb
@@ -1,23 +1,14 @@
 require_relative "test_helper"
 
-$response = ""
-
-App = Rack::Builder.new {
-  use Rack::Committee::Lint, schema: File.read("./test/schema.json")
-  run lambda { |_|
-    [200, {}, [$response]]
-  }
-}
-
 describe Rack::Committee::Lint do
   include Rack::Test::Methods
 
   def app
-    App
+    @app
   end
 
   it "detects an invalid response" do
-    $response = ""
+    @app = new_rack_app("")
     get "/apps"
     assert_equal 500, last_response.status
     assert_match /valid JSON/i, last_response.body
@@ -26,7 +17,7 @@ describe Rack::Committee::Lint do
   it "detects missing keys in response" do
     data = valid_app
     data.delete("name")
-    $response = MultiJson.encode(data)
+    @app = new_rack_app(MultiJson.encode(data))
     get "/apps"
     assert_equal 500, last_response.status
     assert_match /missing keys/i, last_response.body
@@ -35,10 +26,19 @@ describe Rack::Committee::Lint do
   it "detects extra keys in response" do
     data = valid_app
     data.merge!("tier" => "important")
-    $response = MultiJson.encode(data)
+    @app = new_rack_app(MultiJson.encode(data))
     get "/apps"
     assert_equal 500, last_response.status
     assert_match /extra keys/i, last_response.body
+  end
+
+  def new_rack_app(response)
+    Rack::Builder.new {
+      use Rack::Committee::Lint, schema: File.read("./test/schema.json")
+      run lambda { |_|
+        [200, {}, [response]]
+      }
+    }
   end
 
   def valid_app
