d9f8bb484921c0bb66523d86f58f5bd3572bbecd
Brandur <brandur@mutelight.org>
2013-11-08 12:14:15 -0800

    Correct error message checks

--- test/response_validator_test.rb
+++ test/response_validator_test.rb
@@ -12,45 +12,50 @@ describe Rack::Committee::ResponseValidator do
   it "detects missing keys in response" do
     data = ValidApp.dup
     data.delete("name")
-    message = "Missing keys in response: name."
-    assert_raises(Rack::Committee::InvalidResponse, message) do
+    e = assert_raises(Rack::Committee::InvalidResponse) do
       Rack::Committee::ResponseValidator.new(data, @schema, @schema["app"]).call
     end
+    message = "Missing keys in response: name."
+    assert_equal message, e.message
   end
 
   it "detects extra keys in response" do
     data = ValidApp.dup
     data.merge!("tier" => "important")
-    message = "Extra keys in response: tier."
-    assert_raises(Rack::Committee::InvalidResponse, message) do
+    e = assert_raises(Rack::Committee::InvalidResponse) do
       Rack::Committee::ResponseValidator.new(data, @schema, @schema["app"]).call
     end
+    message = "Extra keys in response: tier."
+    assert_equal message, e.message
   end
 
   it "detects mismatched types" do
     data = ValidApp.dup
     data.merge!("maintenance" => "not-bool")
-    message = %{Invalid type at "maintenance": expected not-bool to be [boolean] (was: string).}
-    assert_raises(Rack::Committee::InvalidResponse, message) do
+    e = assert_raises(Rack::Committee::InvalidResponse) do
       Rack::Committee::ResponseValidator.new(data, @schema, @schema["app"]).call
     end
+    message = %{Invalid type at "maintenance": expected not-bool to be ["boolean"] (was: string).}
+    assert_equal message, e.message
   end
 
   it "detects bad formats" do
     data = ValidApp.dup
     data.merge!("id" => "123")
-    message = %{Invalid format at "id": expected "123" to be "uuid".}
-    assert_raises(Rack::Committee::InvalidResponse, message) do
+    e = assert_raises(Rack::Committee::InvalidResponse) do
       Rack::Committee::ResponseValidator.new(data, @schema, @schema["app"]).call
     end
+    message = %{Invalid format at "id": expected "123" to be "uuid".}
+    assert_equal message, e.message
   end
 
   it "detects bad patterns" do
     data = ValidApp.dup
     data.merge!("name" => "%@!")
-    message = %{Invalid pattern at "name": expected %@! to match (?-mix:^[a-z][a-z0-9-]{3,30}$).}
-    assert_raises(Rack::Committee::InvalidResponse, message) do
+    e = assert_raises(Rack::Committee::InvalidResponse) do
       Rack::Committee::ResponseValidator.new(data, @schema, @schema["app"]).call
     end
+    message = %{Invalid pattern at "name": expected %@! to match "(?-mix:^[a-z][a-z0-9-]{3,30}$)".}
+    assert_equal message, e.message
   end
 end
