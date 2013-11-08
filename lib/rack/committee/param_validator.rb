bd88dac0c468a2ca86c2141077bb41d024218ecf
Brandur <brandur@mutelight.org>
2013-11-08 11:28:52 -0800

    Format and pattern paramter validation

--- lib/rack/committee/param_validator.rb
+++ lib/rack/committee/param_validator.rb
@@ -1,16 +1,99 @@
 module Rack::Committee
   class ParamValidator
-    def initialize(params, link_schema)
+    def initialize(params, schema, link_schema)
       @params = params
+      @schema = schema
       @link_schema = link_schema
     end
 
     def call
       detect_missing!
       detect_extra!
+      check_data!
     end
 
     private
+
+    def check_data!
+      @link_schema["schema"]["properties"].each do |key, value|
+        # don't try to check this unless it was actually specificed
+        next unless @params.key?(key)
+
+        match = false
+        definitions = find_definitions(value["$ref"])
+
+        # try to match data against any possible definition
+        definitions.each do |definition|
+          if check_type(definition["type"], @params[key], key) &&
+            check_format(definition["format"], @params[key], key) &&
+            check_pattern(definition["pattern"], @params[key], key)
+            match = true
+            next
+          end
+        end
+
+        # if nothing was matched, throw error according to first definition
+        if !match && definition = definitions.first
+          check_type!(definition["type"], @params[key], key)
+          check_format!(definition["format"], @params[key], key)
+          check_pattern!(definition["pattern"], @params[key], key)
+        end
+      end
+    end
+
+    def check_pattern(pattern, value, key)
+      !pattern || value =~ pattern
+    end
+
+    def check_pattern!(pattern, value, key)
+      unless check_pattern(pattern, value, key)
+        raise InvalidParams,
+          %{Invalid pattern for key "#{key}": expected #{value} to match #{pattern}.}
+      end
+    end
+
+    def check_format(format, value, key)
+      case format
+      when "date-time"
+        value =~ /^(\d{4})-(\d{2})-(\d{2})T(\d{2})\:(\d{2})\:(\d{2})[+-](\d{2})\:(\d{2})$/
+      when "email"
+        value =~ /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/i
+      when "uuid"
+        value =~ /^[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}$/
+      else
+        true
+      end
+    end
+
+    def check_format!(format, value, key)
+      unless check_format(format, value, key)
+        raise InvalidParams,
+          %{Invalid format for key "#{key}": expected "#{value}" to be "#{format}".}
+      end
+    end
+
+    def check_type(types, value, key)
+      type = case value
+      when NilClass
+        "null"
+      when TrueClass, FalseClass
+        "boolean"
+      when Fixnum
+        "integer"
+      when String
+        "string"
+      else
+        "unknown"
+      end
+      types.include?(type)
+    end
+
+    def check_type!(types, value, key)
+      unless check_type(types, value, key)
+        raise InvalidParams,
+          %{Invalid type for key "#{key}": expected #{value} to be #{types}.}
+      end
+    end
 
     def detect_extra!
       extra = @params.keys - all_keys
@@ -23,6 +106,15 @@ module Rack::Committee
       missing = required_keys - @params.keys
       if missing.count > 0
         raise InvalidParams.new("Require params: #{missing.join(', ')}.")
+      end
+    end
+
+    def find_definitions(ref)
+      definition = @schema.find(ref)
+      if definition["anyOf"]
+        definition["anyOf"].map { |r| @schema.find(r["$ref"]) }
+      else
+        [definition]
       end
     end
 
--- test/param_validator_test.rb
+++ test/param_validator_test.rb
@@ -2,22 +2,22 @@ require_relative "test_helper"
 
 describe Rack::Committee::ParamValidator do
   before do
-    data = MultiJson.decode(File.read("./test/data/schema.json"))
-    @link_schema = data["definitions"]["app-transfer"]["links"][0]
+    @schema = Rack::Committee::Schema.new(File.read("./test/data/schema.json"))
+    @link_schema = @schema["app-transfer"]["links"][0]
   end
 
   it "passes through a valid request" do
     params = {
-      "app" => "www",
+      "app" => "heroku-api",
       "recipient" => "owner@heroku.com",
     }
-    Rack::Committee::ParamValidator.new(params, @link_schema).call
+    Rack::Committee::ParamValidator.new(params, @schema, @link_schema).call
   end
 
   it "detects a missing parameter" do
     message = "Missing params: app, recipient."
-    assert_raises(Rack::Committee::InvalidParams) do
-      Rack::Committee::ParamValidator.new({}, @link_schema).call
+    assert_raises(Rack::Committee::InvalidParams, message) do
+      Rack::Committee::ParamValidator.new({}, @schema, @link_schema).call
     end
   end
 
@@ -27,7 +27,40 @@ describe Rack::Committee::ParamValidator do
     }
     message = "Unknown params: cloud."
     assert_raises(Rack::Committee::InvalidParams, message) do
-      Rack::Committee::ParamValidator.new(params, @link_schema).call
+      Rack::Committee::ParamValidator.new(params, @schema, @link_schema).call
+    end
+  end
+
+  it "detects a parameter of the wrong type" do
+    params = {
+      "app" => "heroku-api",
+      "recipient" => 123,
+    }
+    message = %{Invalid type for key "recipient": expected 123 to be ["string"].}
+    assert_raises(Rack::Committee::InvalidParams, message) do
+      Rack::Committee::ParamValidator.new(params, @schema, @link_schema).call
+    end
+  end
+
+  it "detects a parameter of the wrong format" do
+    params = {
+      "app" => "heroku-api",
+      "recipient" => "not-email",
+    }
+    message = %{Invalid format for key "recipient": expected "not-email" to be "email".}
+    assert_raises(Rack::Committee::InvalidParams, message) do
+      Rack::Committee::ParamValidator.new(params, @schema, @link_schema).call
+    end
+  end
+
+  it "detects a parameter of the wrong pattern" do
+    params = {
+      "name" => "%@!"
+    }
+    message = %{Invalid pattern for key "name": expected %@! to match (?-mix:^[a-z][a-z0-9-]{3,30}$).}
+    link_schema = @schema["app"]["links"][0]
+    assert_raises(Rack::Committee::InvalidParams, message) do
+      Rack::Committee::ParamValidator.new(params, @schema, link_schema).call
     end
   end
 end
