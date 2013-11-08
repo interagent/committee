2089e10a71069e87d6d696bbc20c3a3224d2d28b
Brandur <brandur@mutelight.org>
2013-11-07 20:07:25 -0800

    Hate minitest

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,7 @@
-require "minitest/autorun"
-require 'minitest/pride'
+require "minitest"
 require "minitest/spec"
+require 'minitest/pride'
+require "minitest/autorun"
 
 require "bundler/setup"
 Bundler.require(:development)
