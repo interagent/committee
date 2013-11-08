ffaaee28700adbf93d0ddb4994218f0b8b4279ac
Brandur <brandur@mutelight.org>
2013-11-07 20:39:11 -0800

    Fix spacing

--- lib/rack/committee/stub.rb
+++ lib/rack/committee/stub.rb
@@ -5,7 +5,7 @@ module Rack::Committee
       _, type_schema = @router.routes_request?(request)
       if type_schema
         data = ResponseGenerator.new(@schema, type_schema).call
-        str = MultiJson.encode(data, pretty:true)
+        str = MultiJson.encode(data, pretty: true)
         [200, {"Content-Type" => "application/json"}, [str]]
       else
         @app.call(env)
