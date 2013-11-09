a8024da59fbd9324546d9e04d10d3dea076d4bc2
Brandur <brandur@mutelight.org>
2013-11-09 03:22:25 -0800

    Add cache to stub

--- lib/rack/committee/stub.rb
+++ lib/rack/committee/stub.rb
@@ -1,14 +1,32 @@
 module Rack::Committee
   class Stub < Base
+    def initialize(app, options={})
+      super
+      @cache = {}
+    end
+
     def call(env)
       request = Rack::Request.new(env)
       link_schema, type_schema = @router.routes_request?(request)
       if type_schema
-        data = ResponseGenerator.new(@schema, type_schema, link_schema).call
-        str = MultiJson.encode(data, pretty: true)
+        str = cache(link_schema["method"], link_schema["href"]) do
+          data = ResponseGenerator.new(@schema, type_schema, link_schema).call
+          MultiJson.encode(data, pretty: true)
+        end
         [200, {"Content-Type" => "application/json"}, [str]]
       else
         @app.call(env)
+      end
+    end
+
+    private
+
+    def cache(method, href)
+      key = "#{method}+#{href}"
+      if @cache[key]
+        @cache[key]
+      else
+        @cache[key] = yield
       end
     end
   end
