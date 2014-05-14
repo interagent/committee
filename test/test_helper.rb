0d718b48a427edbd2e88778aed14fcf0886d6b86
Brandur <brandur@mutelight.org>
2014-05-14 15:03:42 +0200

    Vastly simplify the test schema; this all gets tested elsewhere

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -9,41 +9,6 @@ Bundler.require(:development)
 require_relative "../lib/committee"
 
 ValidApp = {
-  "archived_at"                    => "2012-01-01T12:00:00Z",
-  "buildpack_provided_description" => "Ruby/Rack",
-  "created_at"                     => "2012-01-01T12:00:00Z",
-  "git_url"                        => "git@heroku.com/example.git",
-  "id"                             => "01234567-89ab-cdef-0123-456789abcdef",
-  "maintenance"                    => false,
-  "name"                           => "example",
-  "owner"                          => {
-    "email"                        => "username@example.com",
-    "id"                           => "01234567-89ab-cdef-0123-456789abcdef"
-  },
-  "region"                         => {
-    "id"                           => "01234567-89ab-cdef-0123-456789abcdef",
-    "name"                         => "us"
-  },
-  "released_at"                    => "2012-01-01T12:00:00Z",
-  "repo_size"                      => 0,
-  "slug_size"                      => 0,
-  "stack"                          => {
-    "id"                           => "01234567-89ab-cdef-0123-456789abcdef",
-    "name"                         => "cedar"
-  },
-  "updated_at"                     => "2012-01-01T12:00:00Z",
-  "web_url"                        => "http://example.herokuapp.com"
-}.freeze
-
-ValidAccount = {
-  "allow_tracking" => true,
-  "beta"           => true,
-  "created_at"     => "2012-01-01T12:00:00Z",
-  "email"          => "username@example.com",
-  "id"             => "01234567-89ab-cdef-0123-456789abcdef",
-  "last_login"     => "2012-01-01T12:00:00Z",
-  "updated_at"     => "2012-01-01T12:00:00Z",
-  "verified"       => true,
-  "flags"          => ["foo", "bar"],
-  "credit_cards"   => [{"account_number" => "1234-1234-1234-1234", "name" => "Rodney Mullen", "security_code" => 123}]
+  "maintenance" => false,
+  "name"        => "example",
 }.freeze
