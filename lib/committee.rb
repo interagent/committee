88bcbd24e5f63b79d65c4b58839532c86db041a5
Brandur <brandur@mutelight.org>
2014-05-14 10:33:18 +0200

    Remove Committee::Schema (replaced by json_schema)

--- lib/committee.rb
+++ lib/committee.rb
@@ -9,7 +9,6 @@ require_relative "committee/request_unpacker"
 require_relative "committee/response_generator"
 require_relative "committee/response_validator"
 require_relative "committee/router"
-require_relative "committee/schema"
 
 require_relative "committee/middleware/base"
 require_relative "committee/middleware/request_validation"
