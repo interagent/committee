fde753d74250c22a92d48f8c3155178e319fe944
Brandur <brandur@mutelight.org>
2014-05-14 14:46:37 +0200

    Stop using /app-transfers

--- test/middleware/request_validation_test.rb
+++ test/middleware/request_validation_test.rb
@@ -10,25 +10,28 @@ describe Committee::Middleware::RequestValidation do
   it "passes through a valid request" do
     @app = new_rack_app
     params = {
-      "app" => "heroku-api",
-      "recipient" => "owner@heroku.com",
+      "name" => "cloudnasium"
     }
     header "Content-Type", "application/json"
-    post "/account/app-transfers", MultiJson.encode(params)
+    post "/apps", MultiJson.encode(params)
     assert_equal 200, last_response.status
   end
 
   it "detects an invalid Content-Type" do
     @app = new_rack_app
     header "Content-Type", "application/whats-this"
-    post "/account/app-transfers", "{}"
+    params = {
+      "name" => "cloudnasium"
+    }
+    post "/apps", MultiJson.encode(params)
     assert_equal 400, last_response.status
+    assert_match /unsupported content-type/i, last_response.body
   end
 
   it "rescues JSON errors" do
     @app = new_rack_app
     header "Content-Type", "application/json"
-    post "/account/app-transfers", "{x:y}"
+    post "/apps", "{x:y}"
     assert_equal 400, last_response.status
     assert_match /valid json/i, last_response.body
   end
@@ -36,11 +39,10 @@ describe Committee::Middleware::RequestValidation do
   it "takes a prefix" do
     @app = new_rack_app(prefix: "/v1")
     params = {
-      "app" => "heroku-api",
-      "recipient" => "owner@heroku.com",
+      "name" => "cloudnasium"
     }
     header "Content-Type", "application/json"
-    post "/v1/account/app-transfers", MultiJson.encode(params)
+    post "/v1/apps", MultiJson.encode(params)
     assert_equal 200, last_response.status
   end
 
@@ -48,11 +50,10 @@ describe Committee::Middleware::RequestValidation do
     mock(Committee).warn_deprecated.with_any_args
     @app = new_rack_app(schema: File.read("./test/data/schema.json"))
     params = {
-      "app" => "heroku-api",
-      "recipient" => "owner@heroku.com",
+      "name" => "cloudnasium"
     }
     header "Content-Type", "application/json"
-    post "/v1/account/app-transfers", MultiJson.encode(params)
+    post "/apps", MultiJson.encode(params)
     assert_equal 200, last_response.status
   end
 
