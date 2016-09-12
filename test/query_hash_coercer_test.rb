b321a687cb19159df6e4cb460587dffb0a8696a3
Michael Sauter <michael.sauter@experteer.com>
2016-09-12 08:18:52 +0200

    Cover more invalid param values with tests

--- test/query_hash_coercer_test.rb
+++ test/query_hash_coercer_test.rb
@@ -14,37 +14,50 @@ describe Committee::QueryHashCoercer do
     assert_equal({}, call(data))
   end
 
-  it "simply skips string" do
+  it "skips values for string param" do
     data = {"name" => "foo"}
     assert_equal({}, call(data))
   end
 
-  it "coerces valid boolean value" do
+  it "coerces valid values for boolean param" do
     data = {"deleted" => "true"}
     assert_equal({"deleted" => true}, call(data))
     data = {"deleted" => "false"}
     assert_equal({"deleted" => false}, call(data))
   end
 
-  it "coerces valid integer value" do
+  it "skips invalid values for boolean param" do
+    data = {"deleted" => "foo"}
+    assert_equal({}, call(data))
+  end
+
+  it "coerces valid values for integer param" do
     data = {"per_page" => "3"}
     assert_equal({"per_page" => 3}, call(data))
+  end
+
+  it "skips invalid values for integer param" do
     data = {"per_page" => "3.5"}
     assert_equal({}, call(data))
     data = {"per_page" => "false"}
     assert_equal({}, call(data))
+    data = {"per_page" => ""}
+    assert_equal({}, call(data))
   end
 
-  it "coerces valid number value" do
+  it "coerces valid values for number param" do
     data = {"threshold" => "3"}
     assert_equal({"threshold" => 3.0}, call(data))
     data = {"threshold" => "3.5"}
     assert_equal({"threshold" => 3.5}, call(data))
+  end
+
+  it "skips invalid values for number param" do
     data = {"threshold" => "false"}
     assert_equal({}, call(data))
   end
 
-  it "coerces valid null value" do
+  it "coerces valid values for null param" do
     data = {"threshold" => ""}
     assert_equal({"threshold" => nil}, call(data))
   end
