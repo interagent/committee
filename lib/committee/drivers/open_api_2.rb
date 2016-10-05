0ed09a0c6592eb346e7b84b7ed8a811b1696ee91
Brandur <brandur@mutelight.org>
2016-10-05 11:46:52 -0600

    Check in very preliminary OpenAPI 2 driver

--- lib/committee/drivers/open_api_2.rb
+++ lib/committee/drivers/open_api_2.rb
@@ -1,4 +1,16 @@
 module Committee::Drivers
   class OpenAPI2
+    def build_routes(schema)
+    end
+
+    def parse(schema)
+      schema = JsonSchema.parse!(schema)
+      schema.expand_references!
+      schema
+    end
+
+    # Link abstracts an API link specifically for OpenAPI 2.
+    class Link
+    end
   end
 end
