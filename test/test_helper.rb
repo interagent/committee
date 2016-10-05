b1174365e880bfd4cc137ae30d2b298436ca065c
Brandur <brandur@mutelight.org>
2016-10-05 12:02:46 -0600

    Undo caching: it's nice to be able to manipulate these structures

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -13,11 +13,9 @@ ValidApp = {
 }.freeze
 
 def hyper_schema_data
-  @hyper_schema_data ||=
-    JSON.parse(File.read("./test/data/hyperschema/heroku.json"))
+  JSON.parse(File.read("./test/data/hyperschema/heroku.json"))
 end
 
 def open_api_2_data
-  @open_api_2_data ||=
-    JSON.parse(File.read("./test/data/openapi2/petstore-expanded.json"))
+  JSON.parse(File.read("./test/data/openapi2/petstore-expanded.json"))
 end
