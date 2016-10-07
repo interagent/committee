c1fef26fb97b1dddfb3951c52b7511d5cb1699a1
Brandur <brandur@mutelight.org>
2016-10-07 14:24:39 -0600

    Add basic test for committee-stub

---
+++ test/bin_test.rb
@@ -0,0 +1,20 @@
+require_relative "test_helper"
+
+#
+# The purpose of this sets of tests is just to include our Ruby executables
+# where possible so that we can get very basic sanity checks on their syntax
+# (which is something that of course Ruby can't do by default).
+#
+# We can do this without actually executing them because they're gated by `if
+# $0 == __FILE__` statements.
+#
+
+describe "executables in bin/" do
+  before do
+    @bin_dir = File.expand_path("../../bin", __FILE__)
+  end
+
+  it "has roughly valid Ruby structure for committee-stub" do
+    load File.join(@bin_dir, "committee-stub")
+  end
+end
