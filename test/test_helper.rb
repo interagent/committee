fcba45173a984f0633ea1c96168f829f01960ea1
Brandur <brandur@mutelight.org>
2016-10-08 13:28:13 -0600

    Don't include bin/ in converage calculations

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,12 +1,16 @@
 require 'simplecov'
 SimpleCov.start do
+  # We do our utmost to test our executables by modularizing them into testable
+  # pieces, but testing them to completion is nearly impossible as far as I can
+  # tell, so include them in our tests but don't calculate coverage.
+  add_filter "/bin/"
+
   add_filter "/test/"
 end
 
 require "minitest"
 require "minitest/spec"
 require "minitest/autorun"
-#require "pry-rescue/minitest"
 require "rack/test"
 require "rr"
 
