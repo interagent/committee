c6b677d2fd0c1672f0c3f5dc4d23e9caf8230a03
ota42y <ota42y@gmail.com>
2017-05-10 20:56:45 +0900

    :tada: support recursive coerce

--- lib/committee/parameter_coercer.rb
+++ lib/committee/parameter_coercer.rb
@@ -5,31 +5,73 @@ module Committee
       @schema = schema
 
       @coerce_date_times = options.fetch(:coerce_date_times, false)
+      @coerce_recursive = options.fetch(:coerce_recursive, false)
     end
 
     def call
-      coerced = {}
-      return coerced unless @schema.respond_to?(:properties)
+      return {} unless @schema.respond_to?(:properties)
 
-      @schema.properties.each do |k, s|
-        original_val = @params[k]
-
-        unless original_val.nil?
-          s.type.each do |to_type|
-            if @coerce_date_times && to_type == "string" && s.format == "date-time"
-              coerce_val = parse_date_time(original_val)
-              coerced[k] = coerce_val if coerce_val
-            end
-
-            break if coerced.key?(k)
-          end
-        end
-      end
-
-      coerced
+      coercer_value(@params, @schema.properties)
     end
 
     private
+
+      def coercer_value(params, properties)
+        coerced = {}
+
+        properties.each do |k, s|
+          original_val = params[k]
+
+          unless original_val.nil?
+            s.type.each do |to_type|
+              if @coerce_date_times && to_type == "string" && s.format == "date-time"
+                coerce_val = parse_date_time(original_val)
+                coerced[k] = coerce_val if coerce_val
+              end
+
+              if (@coerce_recursive && to_type == "array") || (@coerce_recursive && to_type == "object")
+                coerce_val = coerce_data_recursive(original_val, to_type, s)
+                coerced[k] = coerce_val unless coerce_val.empty?
+              end
+
+              break if coerced.key?(k)
+            end
+          end
+        end
+
+        coerced
+      end
+
+      def coerce_data_recursive(original_val, to_type, schema)
+        return coerce_array_data(original_val, schema) if to_type == "array"
+        return coerce_object_data(original_val, schema) if to_type == "object"
+        nil # bug?
+      end
+
+      def coerce_object_data(original_val, schema)
+        return {} unless schema.respond_to?(:properties)
+
+        coerce_val = coercer_value(original_val, schema.properties)
+        original_val.merge(coerce_val)
+      end
+
+      def coerce_array_data(original_val, schema)
+        return [] unless schema.respond_to?(:items)
+        return [] unless original_val.is_a?(Array)
+
+        is_coerced = false
+        coerced_array = []
+        original_val.each do |d|
+          coerced = coercer_value(d, schema.items.properties)
+
+          is_coerced =  true unless coerced.empty?
+          coerced_array << d.merge(coerced)
+        end
+
+        return [] unless is_coerced # did't coerce
+        coerced_array
+      end
+
       def parse_date_time(original_val)
         begin
           DateTime.parse(original_val)
--- test/parameter_coercer_test.rb
+++ test/parameter_coercer_test.rb
@@ -24,6 +24,72 @@ describe Committee::ParameterCoercer do
     assert_nil converted_params["update_time"]
   end
 
+  it "pass array property" do
+    params = {
+        "array_property" => [
+            {
+                "update_time" => "2016-04-01T16:00:00.000+09:00",
+                "per_page" => 1,
+                "nested_coercer_object" => {
+                    "update_time" => "2016-04-01T16:00:00.000+09:00",
+                    "threshold" => 1.5
+                },
+                "nested_no_coercer_object" => {
+                    "per_page" => 1,
+                    "threshold" => 1.5
+                },
+                "nested_coercer_array" => [
+                    {
+                        "update_time" => "2016-04-01T16:00:00.000+09:00",
+                        "threshold" => 1.5
+                    }
+                ],
+                "nested_no_coercer_array" => [
+                    {
+                        "per_page" => 1,
+                        "threshold" => 1.5
+                    }
+                ]
+            },
+            {
+                "update_time" => "2016-04-01T16:00:00.000+09:00",
+                "per_page" => 1,
+                "threshold" => 1.5
+            },
+            {
+                "threshold" => 1.5,
+                "per_page" => 1
+            }
+        ],
+    }
+    converted_params = call(params, coerce_date_times: true, coerce_recursive: true)
+
+    first_data = converted_params["array_property"][0]
+    assert_kind_of DateTime, first_data["update_time"]
+    assert_kind_of Integer, first_data["per_page"]
+
+    second_data = converted_params["array_property"][1]
+    assert_kind_of DateTime, second_data["update_time"]
+    assert_kind_of Integer, second_data["per_page"]
+    assert_kind_of Float, second_data["threshold"]
+
+    third_data = converted_params["array_property"][1]
+    assert_kind_of Integer, third_data["per_page"]
+    assert_kind_of Float, third_data["threshold"]
+
+    assert_kind_of DateTime, first_data["nested_coercer_object"]["update_time"]
+    assert_kind_of Float, first_data["nested_coercer_object"]["threshold"]
+
+    assert_kind_of Integer, first_data["nested_no_coercer_object"]["per_page"]
+    assert_kind_of Float, first_data["nested_no_coercer_object"]["threshold"]
+
+    assert_kind_of DateTime, first_data["nested_coercer_array"].first["update_time"]
+    assert_kind_of Float, first_data["nested_coercer_array"].first["threshold"]
+
+    assert_kind_of Integer, first_data["nested_no_coercer_array"].first["per_page"]
+    assert_kind_of Float, first_data["nested_no_coercer_array"].first["threshold"]
+  end
+
   private
 
   def call(params, options={})
