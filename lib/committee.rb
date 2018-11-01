3d6463e53e52cab0bc54d2b197a672c148d5d59f
ota42y <ota42y@gmail.com>
2018-11-01 19:04:58 +0900

    Refactoring RequestValidation
    Now RequestValidation is deppend on JSON Hyper-Schema structure.
    I want to add OpenAPI3 feature so I move HyperSchema Validate class and I'll implement OpenAPI3 Validete class for request validation.

--- lib/committee.rb
+++ lib/committee.rb
@@ -21,6 +21,9 @@ require_relative "committee/middleware/request_validation"
 require_relative "committee/middleware/response_validation"
 require_relative "committee/middleware/stub"
 
+require_relative "committee/schema_validator/hyper_schema"
+require_relative "committee/schema_validator/option"
+
 require_relative "committee/bin/committee_stub"
 
 require_relative "committee/test/methods"
---
+++ lib/committee/schema_validator/hyper_schema.rb
@@ -0,0 +1,75 @@
+class Committee::SchemaValidator
+  class HyperSchema
+    attr_reader :link, :param_matches, :validator_option
+
+    def initialize(router, request, validator_option)
+      @link, @param_matches = router.find_request_link(request)
+      @validator_option = validator_option
+    end
+
+    def call(request)
+
+      # Attempts to coerce parameters that appear in a link's URL to Ruby
+      # types that can be validated with a schema.
+      param_matches_hash = coerce_path_params ? coerce_path_params : {}
+
+      # Attempts to coerce parameters that appear in a query string to Ruby
+      # types that can be validated with a schema.
+      coerce_query_params(request) if validator_option.coerce_query_params
+
+      request_unpack(request)
+
+      request.env[validator_option.params_key].merge!(param_matches_hash) if param_matches_hash
+
+      request_validate(request)
+      parameter_coerce!(request, link, validator_option.params_key)
+      parameter_coerce!(request, link, "rack.request.query_hash") if link_exist? && !request.GET.nil? && !link.schema.nil?
+    end
+
+    def coerce_path_params
+      return unless link_exist?
+
+      Committee::StringParamsCoercer.new(param_matches, link.schema, coerce_recursive: validator_option.coerce_recursive).call!
+      param_matches
+    end
+
+    def coerce_query_params(request)
+      return unless link_exist?
+      return if request.GET.nil? || link.schema.nil?
+
+      Committee::StringParamsCoercer.new(request.GET, link.schema, coerce_recursive: validator_option.coerce_recursive).call!
+    end
+
+    def request_unpack(request)
+      request.env[validator_option.params_key], request.env[validator_option.headers_key] = Committee::RequestUnpacker.new(
+          request,
+          allow_form_params:  validator_option.allow_form_params,
+          allow_query_params: validator_option.allow_query_params,
+          coerce_form_params: validator_option.coerce_form_params,
+          optimistic_json:    validator_option.optimistic_json,
+          schema:             link ? link.schema : nil
+      ).call
+    end
+
+    def request_validate(request)
+      return unless link_exist?
+      validator = Committee::RequestValidator.new(link, check_content_type: validator_option.check_content_type, check_header: validator_option.check_header)
+      validator.call(request, request.env[validator_option.params_key], request.env[validator_option.headers_key])
+    end
+
+    def parameter_coerce!(request, link, coerce_key)
+      return unless link_exist?
+
+      Committee::ParameterCoercer.
+          new(request.env[coerce_key],
+              link.schema,
+              coerce_date_times: validator_option.coerce_date_times,
+              coerce_recursive: validator_option.coerce_recursive).
+          call!
+    end
+
+    def link_exist?
+      !link.nil?
+    end
+  end
+end
