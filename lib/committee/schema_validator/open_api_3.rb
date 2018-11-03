96d28311381aa599731d6f37f579edc1467e11c9
ota42y <ota42y@gmail.com>
2018-11-03 17:51:53 +0900

    OpenAPI3 support strict option

--- lib/committee/schema_validator/open_api_3.rb
+++ lib/committee/schema_validator/open_api_3.rb
@@ -1,11 +1,18 @@
 class Committee::SchemaValidator
   class OpenAPI3
     def initialize(router, request, validator_option)
-      # TODO: implements
+      @router = router
+      @request = request
+      @path_object = router.path_object(request)
+      @validator_option = validator_option
     end
 
     def request_validate(_request)
       # TODO: implements
+    end
+
+    def link_exist?
+      !@path_object.nil?
     end
   end
 end
--- lib/committee/schema_validator/open_api_3/router.rb
+++ lib/committee/schema_validator/open_api_3/router.rb
@@ -1,15 +1,24 @@
 module Committee
   class SchemaValidator::OpenAPI3::Router
     def initialize(schema, validator_option)
-      # TODO: implements
+      @schema = schema
+      @prefix = validator_option.prefix
+      @validator_option = validator_option
     end
 
     def includes_request?(request)
-      # TODO: implements
+      return true unless @prefix
+      #path = request.path
+      #path.start_with?(@prefix)
     end
 
     def build_schema_validator(request)
-      # TODO: implements
+      Committee::SchemaValidator::OpenAPI3.new(self, request, @validator_option)
+    end
+
+    def path_object(request)
+      path = request.path
+      @schema.path_object(path)
     end
   end
 end
