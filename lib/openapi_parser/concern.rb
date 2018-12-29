9eb225b1b935cd897e84c17a681088e4911ce8d3
ota42y <ota42y@gmail.com>
2018-12-29 15:30:03 +0900

    support other content type

--- lib/openapi_parser/concern.rb
+++ lib/openapi_parser/concern.rb
@@ -1,3 +1,4 @@
 require_relative 'concerns/parser'
 require_relative 'concerns/findable'
 require_relative 'concerns/expandable'
+require_relative 'concerns/media_type_selectable'
--- lib/openapi_parser/schemas/request_body.rb
+++ lib/openapi_parser/schemas/request_body.rb
@@ -2,16 +2,23 @@
 
 module OpenAPIParser::Schemas
   class RequestBody < Base
+    include OpenAPIParser::MediaTypeSelectable
+
+    # @!attribute [r] description
+    #   @return [String] description data
+    # @!attribute [r] required
+    #   @return [Boolean] required bool data
     openapi_attr_values :description, :required
 
     # @!attribute [r] content
-    #   @return [Hash{String => MediaType}, nil]
+    #   @return [Hash{String => MediaType}, nil] content type to MediaType object
     openapi_attr_hash_object :content, MediaType, reference: false
 
-    def validate_request_body(_content_type, params, options)
-      # TODO: now support application/json only :(
-
-      media_type = content['application/json']
+    # @param [String] content_type
+    # @param [Hash] params
+    # @param [OpenAPIParser::SchemaValidator::Options] options
+    def validate_request_body(content_type, params, options)
+      media_type = select_media_type(content_type, content)
       return params unless media_type
 
       media_type.validate_parameter(params, options)
