afc38c4b993c7e84b125948c9ced9ebd0dfea728
ota42y <ota42y@gmail.com>
2018-12-29 16:25:43 +0900

    add loader test

---
+++ spec/openapi_parser/concerns/schema_loader/base_spec.rb
@@ -0,0 +1,9 @@
+require_relative '../../../spec_helper'
+
+RSpec.describe OpenAPIParser::SchemaLoader::Base do
+  describe '#load_data' do
+    subject { OpenAPIParser::SchemaLoader::Base.new(nil, {}).load_data(nil, nil) }
+
+    it { expect{subject}.to raise_error(StandardError).with_message('need implement')}
+  end
+end
