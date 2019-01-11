0dd59510cc7485900bd4580664405c491451856c
Akira Matsuda <ronnie@dio.jp>
2019-01-11 17:26:58 +0900

    Incorrect error and warning messages
    the :schema option here expects a Schema object since 2.0

--- lib/committee/middleware/base.rb
+++ lib/committee/middleware/base.rb
@@ -68,8 +68,8 @@ module Committee::Middleware
       # Expect the type we want by now. If we don't have it, the user passed
       # something else non-standard in.
       if !schema.is_a?(Committee::Drivers::Schema)
-        raise ArgumentError, "Committee: schema expected to be a hash or " \
-          "an instance of Committee::Drivers::Schema."
+        raise ArgumentError, "Committee: schema expected to be an instance of " \
+          "Committee::Drivers::Schema."
       end
 
       schema
@@ -77,17 +77,17 @@ module Committee::Middleware
 
     def warn_json_schema_deprecated
       Committee.warn_deprecated("Committee: passing a JsonSchema::Schema to schema " \
-        "option is deprecated; please send a driver object instead.")
+        "option is deprecated; please send an instance of Committee::Drivers::Schema instead.")
     end
 
     def warn_hash_deprecated
       Committee.warn_deprecated("Committee: passing a hash to schema " \
-        "option is deprecated; please send a driver object instead.")
+        "option is deprecated; please send an instance of Committee::Drivers::Schema instead.")
     end
 
     def warn_string_deprecated
       Committee.warn_deprecated("Committee: passing a string to schema " \
-        "option is deprecated; please send a driver object instead.")
+        "option is deprecated; please send an instance of Committee::Drivers::Schema instead.")
     end
   end
 end
