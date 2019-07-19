7e561cf394da320ea8b848b366ddd89f5d13c284
chibicco <chibicco160.kino@gmail.com>
2019-07-19 13:37:39 +0900

    Update assert_schema_conform
    divided the processing of assert_schema_conform into assert_request_schema_confirm and assert_response_schema_confirm.
    assert_schema_conform became deprecation, backward compatible with the old_assert_behavior option.

--- lib/committee/test/methods.rb
+++ lib/committee/test/methods.rb
@@ -1,18 +1,22 @@
 module Committee::Test
   module Methods
     def assert_schema_conform
-      @schema ||= Committee::Middleware::Base.get_schema(committee_options)
-      @router ||= @schema.build_router(committee_options)
+      assert_request_schema_confirm unless old_behavior
+      assert_response_schema_confirm
+    end
 
-      v = @router.build_schema_validator(request_object)
+    def assert_request_schema_confirm
+      schema_validator.request_validate(request_object)
+    end
 
-      unless v.link_exist?
+    def assert_response_schema_confirm
+      unless schema_validator.link_exist?
         response = "`#{request_object.request_method} #{request_object.path_info}` undefined in schema."
         raise Committee::InvalidResponse.new(response)
       end
 
       status, headers, body = response_data
-      v.response_validate(status, headers, [body], true) if validate_response?(status)
+      schema_validator.response_validate(status, headers, [body], true) if validate_response?(status)
     end
 
     def committee_options
@@ -29,6 +33,32 @@ module Committee::Test
 
     def validate_response?(status)
       Committee::Middleware::ResponseValidation.validate?(status, committee_options.fetch(:validate_success_only, false))
+    end
+
+    def schema
+      @schema ||= Committee::Middleware::Base.get_schema(committee_options)
+    end
+
+    def router
+      @router ||= schema.build_router(committee_options)
+    end
+
+    def schema_validator
+      @schema_validator ||= router.build_schema_validator(request_object)
+    end
+
+    def old_behavior
+      old_assert_behavior = committee_options.fetch(:old_assert_behavior, nil)
+      if old_assert_behavior.nil?
+        warn <<-MSG
+        [DEPRECATION] now assert_schema_confirm check response schema only.
+          but we will change check request and response in future major version.
+          so if you want to conform response only, please use assert_response_schema_confirm,
+          or you can suppress this message and keep old behavior by setting old_assert_behavior=true.
+        MSG
+        old_assert_behavior = true
+      end
+      old_assert_behavior
     end
   end
 end
