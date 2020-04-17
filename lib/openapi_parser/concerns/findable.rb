07aad00323432e0a376479d792b979c9cdb902cd
Justin Feng <justin.feng@myob.com>
2020-04-17 22:19:22 +1000

    Prevent SystemStackError on recursive schema reference, close #41

--- lib/openapi_parser/concerns/findable.rb
+++ lib/openapi_parser/concerns/findable.rb
@@ -30,7 +30,10 @@ module OpenAPIParser::Findable
   end
 
   def purge_object_cache
+    return if @purged
+
     @find_object_cache = {}
+    @purged = true
 
     _openapi_all_child_objects.values.each(&:purge_object_cache)
   end
