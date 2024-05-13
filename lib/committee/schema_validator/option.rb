b165473598f097a34604bf39d917c26e7018e915
r7kamura <r7kamura@gmail.com>
2024-05-13 15:31:03 +0900

    Support the overwite version for backward compatibility

--- lib/committee/schema_validator/option.rb
+++ lib/committee/schema_validator/option.rb
@@ -45,7 +45,14 @@ module Committee
         @coerce_recursive                 = options.fetch(:coerce_recursive, true)
         @optimistic_json                  = options.fetch(:optimistic_json, false)
         @parse_response_by_content_type   = options.fetch(:parse_response_by_content_type, true)
-        @parameter_overwrite_by_rails_rule = options.fetch(:parameter_overwrite_by_rails_rule, true)
+
+        @parameter_overwrite_by_rails_rule =
+          if options.key?(:parameter_overwite_by_rails_rule)
+            Committee.warn_deprecated_until_6(true, "The option `parameter_overwite_by_rails_rule` is deprecated. Use `parameter_overwrite_by_rails_rule` instead.")
+            options[:parameter_overwite_by_rails_rule]
+          else
+            options.fetch(:parameter_overwrite_by_rails_rule, true)
+          end
 
         # Boolean options and have a different value by default
         @allow_get_body        = options.fetch(:allow_get_body, schema.driver.default_allow_get_body)
